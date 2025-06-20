f1ef25dd013110035741b0ea7be782202f5f0ad1
monkey-vla <robomonkeyvla@gmail.com>
2025-06-19 22:56:19 -0700

    remove bench

--- cot_bench/request_update.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:30000/update_weights"
-data = {"model_path": "/root/vla-cot/checkpoint"}
-
-response = requests.post(url, json=data)
-response_json = response.json()
-print(response.json())
